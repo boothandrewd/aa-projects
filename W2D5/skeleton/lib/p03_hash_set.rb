6830fe56d4bf04633ef8480de61d2ce8f78c0d8d
Andrew Booth <boothandrewd@gmail.com>
2017-06-09 17:04:25 -0700

    Implement hash map

--- W2D5/skeleton/lib/p03_hash_set.rb
+++ W2D5/skeleton/lib/p03_hash_set.rb
@@ -1,7 +1,7 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -9,18 +9,27 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      resize! if @count + 1 > num_buckets
+      self[key.hash] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    self[key.hash].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +37,12 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self[el.hash] << el
+      end
+    end
   end
 end
--- W2D5/skeleton/lib/p04_linked_list.rb
+++ W2D5/skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+include Enumerable
+
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -15,11 +17,16 @@ class Link
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @next.prev = @prev
+    @prev.next = @next
   end
 end
 
 class LinkedList
   def initialize
+    @sentinel = Link.new
+    @sentinel.next = @sentinel
+    @sentinel.prev = @sentinel
   end
 
   def [](i)
@@ -28,34 +35,66 @@ class LinkedList
   end
 
   def first
+    @sentinel.next
   end
 
   def last
+    @sentinel.prev
   end
 
   def empty?
+    @sentinel.next == @sentinel &&
+      @sentinel.prev == @sentinel
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    each { |link| return true if link.key == key }
+    false
   end
 
   def append(key, val)
+    tail = @sentinel.prev
+
+    link = Link.new(key, val)
+
+    tail.next = link
+    link.next = @sentinel
+
+    @sentinel.prev = link
+    link.prev = tail
   end
 
   def update(key, val)
+    each { |link| link.val = val if link.key == key }
   end
 
   def remove(key)
+    link_val = nil
+    each do |link|
+      if link.key == key
+        link_val = link.val
+        link.remove
+      end
+    end
+
+    link_val
   end
 
   def each
+    link = @sentinel.next
+    until link == @sentinel
+      yield link
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
